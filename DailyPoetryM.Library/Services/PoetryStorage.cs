be3d171b9538abeede05425d4928e246f89b6d34
Fram Herel <dsc5255898@outlook.com>
2022-10-20 18:46:20 +0800

    添加项目文件。

---
+++ DailyPoetryM.Library/Services/PoetryStorage.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using DailyPoetryM.Models;
+using SQLite;
+
+namespace DailyPoetryM.Services;
+
+public class PoetryStorage : IPoetryStorage
+{
+    public const string DbName = "poetrydb.sqlite3";
+    public static readonly string PoetryDbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DbName);
+
+    private SQLiteAsyncConnection? _connection;
+    private SQLiteAsyncConnection Connection => _connection ??= new SQLiteAsyncConnection(PoetryDbPath);
+
+    private readonly IPreferenceStorage _preferenceStorage;
+
+    public PoetryStorage(IPreferenceStorage preferenceStorage)
+    {
+        _preferenceStorage = preferenceStorage;
+    }
+
+    public bool IsInitialized => _preferenceStorage.Get(PoetryStorageConstant.VersionKey, 0) == PoetryStorageConstant.Version;
+
+    public async Task InitializedAsync()
+    {
+        await using var dbFileStream = new FileStream(PoetryDbPath, FileMode.OpenOrCreate);
+        await using var dbAssetStream = typeof(PoetryStorage).Assembly.GetManifestResourceStream(DbName) ??
+            throw new Exception($"Manifest not found: {DbName}");
+        await dbAssetStream.CopyToAsync(dbFileStream);
+
+        _preferenceStorage.Set(PoetryStorageConstant.VersionKey, PoetryStorageConstant.Version);
+    }
+    public async Task<Poetry> GetPoetryAsync(int id) => await Connection.Table<Poetry>().FirstOrDefaultAsync(p => p.Id == id);
+
+    public async Task<IEnumerable<Poetry>> GetPoetriesAsync(Expression<Func<Poetry, bool>> where, int skip, int take) => 
+        await Connection.Table<Poetry>().Where(where).Skip(skip).Take(take).ToListAsync();
+
+    public async Task CloesAsync() => await Connection.CloseAsync();
+}
+
+public static class PoetryStorageConstant
+{
+    public const int Version = 1;
+    public const string VersionKey = nameof(PoetryStorageConstant) + "." + nameof(Version);
+}
---
+++ DailyPoetryM.UnitTest/Services/PoetryStorageTest.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using DailyPoetryM.Models;
+using DailyPoetryM.Services;
+using Moq;
+
+namespace DailyPoetryM.UnitTest.Services;
+
+public class PoetryStorageTest : IDisposable
+{
+    public PoetryStorageTest()
+    {
+        File.Delete(PoetryStorage.PoetryDbPath);
+    }
+
+    public void Dispose()
+    {
+        File.Delete(PoetryStorage.PoetryDbPath);
+    }
+
+    [Fact]
+    public void IsInitialized_Default()
+    {
+        var preferenceStorageMock = new Mock<IPreferenceStorage>();
+        preferenceStorageMock
+            .Setup(p => p.Get(PoetryStorageConstant.VersionKey, 0))
+            .Returns(PoetryStorageConstant.Version);
+        var mockPreferenceStorage = preferenceStorageMock.Object;
+        var poetryStorage = new PoetryStorage(mockPreferenceStorage);
+        Assert.True(poetryStorage.IsInitialized);
+    }
+
+    [Fact]
+    public async Task TestInitializeAsync_Default()
+    {
+        var preferenceStorageMock = new Mock<IPreferenceStorage>();
+        var mockPreferenceStorage = preferenceStorageMock.Object;
+        var poetryStorage = new PoetryStorage(mockPreferenceStorage);
+        Assert.False(File.Exists(PoetryStorage.PoetryDbPath));
+        await poetryStorage.InitializedAsync();
+        Assert.True(File.Exists(PoetryStorage.PoetryDbPath));
+
+        preferenceStorageMock.Verify(p => p.Set(PoetryStorageConstant.VersionKey, PoetryStorageConstant.Version), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetPoetryAsync_Default()
+    {
+        var poetryStorage = await GetInitializedPoetryStorage();
+
+        var poetry = await poetryStorage.GetPoetryAsync(10001);
+        Assert.Equal("临江仙 · 夜归临皋", poetry.Name);
+
+        await poetryStorage.CloesAsync();
+    }
+
+    [Fact]
+    public async Task GetPoetriesAsync_Default()
+    {
+        var poetryStorage = await GetInitializedPoetryStorage();
+
+        var poetries = await poetryStorage.GetPoetriesAsync(Expression.Lambda<Func<Poetry, bool>>(Expression.Constant(true), Expression.Parameter(typeof(Poetry), "p")), 0, int.MaxValue);
+        Assert.Equal(30,poetries.Count());
+        await poetryStorage.CloesAsync();
+    }
+
+    public async Task<PoetryStorage> GetInitializedPoetryStorage()
+    {
+        var preferenceStorageMock = new Mock<IPreferenceStorage>();
+        var mockPreferenceStorage = preferenceStorageMock.Object;
+        var poetryStorage = new PoetryStorage(mockPreferenceStorage);
+        await poetryStorage.InitializedAsync();
+        return poetryStorage;
+    }
+}
